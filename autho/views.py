09873ee8b9133e049218c40e6b5feefda69eb688
u.dukuev <u.dukuev@ase-ec.ru>
2024-06-27 16:42:07 +0300

    п

--- autho/views.py
+++ autho/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Hello1")
+    #return HttpResponse("Hello1")
+    return render(request, 'autho/autho.html', {})
